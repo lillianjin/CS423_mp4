82336d3c0ac018203d6e6532d32d9de46c0ead41
Lu Jin <lillianjin67@gmail.com>
2019-05-02 18:22:50 -0500

    ver 0.1.69

--- mp4.c
+++ mp4.c
@@ -441,7 +441,7 @@ static int mp4_inode_permission(struct inode *inode, int mask)
 		// pr_err("mp4_inode_permission: current cred not found\n");
 		return 0;
 	}
-
+	return 0;
 	curr = (struct mp4_security *)(current_cred()->security);
 	ssid = curr->mp4_flags;
 	// if(ssid == MP4_TARGET_SID && S_ISDIR(inode->i_mode)){
