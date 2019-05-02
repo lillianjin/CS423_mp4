558e4bffd7ff4858d5216d4bf6b87a179ecb65af
Lu Jin <lillianjin67@gmail.com>
2019-05-01 19:47:38 -0500

    ver 0.1.56

--- mp4.c
+++ mp4.c
@@ -183,11 +183,10 @@ static int mp4_cred_prepare(struct cred *new, const struct cred *old,
 {
 	mp4_cred_alloc_blank(new, gfp);
 	// pr_info("mp4 prepare a new credential for modification..");
-	if(!old){
-		return -ENOENT;
-	}
-	if(old->security){
+	if(old && old->security){
 		new -> security = old -> security;
+	} else {
+		mp4_cred_alloc_blank(new, gfp);
 	}
 	return 0;
 }
